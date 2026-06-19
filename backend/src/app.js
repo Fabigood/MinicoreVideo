8131ed4c1eaa41a19a33d1f7202c529780988f0a
Fabio <fiallosfabio@gmail.com>
2026-06-19 01:40:29 -0500

    fix2

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,16 @@ const reporteRoutes = require("./routes/reporteRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
